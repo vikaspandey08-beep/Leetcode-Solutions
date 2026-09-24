e03b8ad02e70616be129f8c99faf95afa358943c
Vikas Pandey <vippandey08@gmail.com>
2026-09-24 13:13:08 +0530

    New Leetcode Problem.

---
+++ Problem3.java
@@ -0,0 +1,12 @@
+class Problem3  {
+    public int sumOfMultiples(int n) {
+        int TotalSum = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
+                 TotalSum += i;
+            }
+        }
+     return TotalSum ;
+}
+}
